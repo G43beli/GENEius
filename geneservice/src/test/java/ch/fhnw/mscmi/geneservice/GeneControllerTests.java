25100a4bb459a56a0b25ad178aa8b2708034a412
GabrielMassaroFHNW <gabriel.massaro@students.fhnw.ch>
2022-04-30 15:54:11 +0200

    exercise maven, part I

---
+++ geneservice/src/test/java/ch/fhnw/mscmi/geneservice/GeneControllerTests.java
@@ -0,0 +1,30 @@
+package ch.fhnw.mscmi.geneservice;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GeneControllerTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void retrieveGeneByIdTest() throws Exception {
+		ResultActions ra = this.mockMvc.perform(get("/geneservice/byid").param("id", "801529"))
+									   .andDo(print())
+									   .andExpect(status().isOk())
+									   .andExpect(jsonPath("$.geneId").value(801529));
+	}
+}
